475a645ec9e8a37f4baf7e5f35c03b5cbed5999e
KShatZ <kirillshats@gmail.com>
2022-05-13 17:31:40 -0400

    Implement session middleware and session store

--- app.js
+++ app.js
@@ -1,22 +1,41 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
 
 const indexRouter = require("./routes/index"),
 	composeRouter = require("./routes/compose"),
 	entryRouter = require("./routes/entry");
 
 
-// App Config
+// ------ App Config ------
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-// Database Config - local
+// ------ Database Config ------
 mongoose.connect("mongodb://localhost:27017/devJournalDB");
 
-// Routing
+// ------ Session Config ------
+const storeOptions = {
+	mongoUrl: process.env.DB_URL,
+	ttl: 60 * 60 * 24 * 7,
+	autoRemove: "interval",
+	autoRemoveInterval: 60 * 24
+}
+
+app.use(session({
+	secret: process.env.SESSION_SECRET,
+	resave: false,
+	saveUninitialized: false,
+	store: MongoStore.create(storeOptions)
+}));
+
+
+// ------ Routing ------
 app.use(indexRouter);
 app.use(composeRouter);
 app.use(entryRouter);
