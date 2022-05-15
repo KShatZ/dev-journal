693c083bd58508e8e9825c8c7cd084f5f85706b3
KShatZ <kirillshats@gmail.com>
2022-05-15 11:36:32 -0400

    Configure passport

---
+++ auth/passport-config.js
@@ -0,0 +1,26 @@
+const Dev = require("../models/dev");
+const passport = require("passport");
+const localStrategy = require("./strategies/local");
+
+
+// ------ Serialization / Deserialization -------
+passport.serializeUser(function (dev, done) {
+    return done(null, dev.id);
+});
+
+passport.deserializeUser(function (devID, done) {
+    Dev.findById(devId, function (err, dev) {
+        if (err) {
+            return done(err);
+        }
+        return done(null, dev);
+    })
+});
+
+
+// ------ Strategy Loading ------ 
+passport.use(localStrategy);
+
+
+
+module.exports = passport;
